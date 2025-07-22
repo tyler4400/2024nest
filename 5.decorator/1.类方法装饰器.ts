596992444f9f417927d9594d2e26fc4c2becd76d
tyler4400 <tyler4400@foxmail.com>
2025-07-22 22:54:20 +0800

    stage3 装饰器规则

--- 5.decorator/1.类方法装饰器.ts
+++ 5.decorator/1.类方法装饰器.ts
@@ -1,4 +1,12 @@
-
+/**
+ *
+ * 新版装饰器需要在tsconfig中注释下面两个配置。
+ * 这两个配置针对的是stage2阶段的提案，注释或者什么都不写之后，默认就是新版stage3
+ * Enable experimental support for legacy experimental decorators.
+ *    "experimentalDecorators": true,
+ *     "emitDecoratorMetadata": true,
+ *
+ */
 type ClassMethodDecorator = (value: Function, context: {
     kind: 'method',//被装饰的值的类型,可以是class method getter setter field accessor(没学) 之一
     name: string,//表示被装饰的值的名称
@@ -6,7 +14,7 @@ type ClassMethodDecorator = (value: Function, context: {
     private: boolean,//表示值是否是私有的类元素
 }) => Function | void
 
-function logged(value, context) {
+function logged(value, context): ClassMethodDecorator {
     console.log('value', value)
     console.log('context', context)
     if (context.kind === 'method') {//说明这是一个类的方法装饰器
